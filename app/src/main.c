f77a2635aafdc804bae2329d77dd29318ef06ecd
Matias Tripode <matiastripode@gmail.com>
2026-08-12 14:36:42 -0400

    Add assignment 2

--- app/src/main.c
+++ app/src/main.c
@@ -1,77 +1,59 @@
+#include <stdint.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
-LOG_MODULE_REGISTER(l1_task, LOG_LEVEL_DBG);
+/*
+Task:
+    Goal: Observe and fix a real race condition
+    Tasks:
+        - Create two equal-priority threads
+        - Both increment a shared counter
+        - Observe inconsistent results
+        - Fix using mutex
+        - Push tag: l2-task1
+*/
+
+LOG_MODULE_REGISTER(l2_task, LOG_LEVEL_DBG);
 
 #define STACK_SIZE 1024
 
-#define PRIO_COOP (-1)
-#define PRIO_HIGH 3
 #define PRIO_MED  5
-#define PRIO_LOW  7
+#define INCREMENTS 1000000
+
+static volatile uint32_t shared_counter;
+K_MUTEX_DEFINE(shared_mutex);
 
 
-void thread_low_fn(void *p1, void *p2, void *p3)
+void thread_fn(void *p1, void *p2, void *p3)
 {
-    int step = 0;
-
-    for (int i = 0; i < 5; i ++) {
-        LOG_INF("T_LOW running %d  tick=%u", step++, k_uptime_get_32());
-        // k_sleep(K_MSEC(300));
-        k_busy_wait(300000);
+    const char *name = k_thread_name_get(k_current_get());
+    LOG_INF("[%s][Start] shared_counter=[%d]  tick=[%u]", name, shared_counter, k_uptime_get_32());
+    // Try to lock the shared_mutex
+    if (k_mutex_lock(&shared_mutex, K_FOREVER) != 0) {
+        LOG_INF("[%s][MUTEX] error trying to lock mutextick=[%u]", name, k_uptime_get_32());
+        return;
     }
-}
-
-void thread_med_fn(void *p1, void *p2, void *p3)
-{
-    int step = 0;
-
-    for (int i = 0; i < 5; i ++) {
-        LOG_INF("T_MED running %d  tick=%u", step++, k_uptime_get_32());
-        // k_sleep(K_MSEC(200));
-        k_busy_wait(200000);
+    for (int i = 0; i < INCREMENTS; i ++) {        
+        shared_counter ++;
     }
-}
-
-void thread_high_fn(void *p1, void *p2, void *p3)
-{
-    int step = 0;
-
-    for (int i = 0; i < 5; i ++) {
-        LOG_INF("T_HIGH running %d  tick=%u", step++, k_uptime_get_32());
-        // k_sleep(K_MSEC(100));
-        k_busy_wait(100000);
-    }
-}
-
-void thread_cooperative_fn(void *p1, void *p2, void *p3)
-{
-    int step = 0;
-
-    for (int i = 0; i < 5; i ++) {
-        LOG_INF("T_COOPERATIVE running %d  tick=%u", step++, k_uptime_get_32());
-        // k_sleep(K_MSEC(100));
-        k_busy_wait(100000);
-    }
+    // Release shared_mutex
+    k_mutex_unlock(&shared_mutex);
+    LOG_INF("[%s][Finish] shared_counter=[%d]  tick=[%u]", name, shared_counter, k_uptime_get_32());
 }
 
 
-K_THREAD_DEFINE(thread_low, STACK_SIZE, thread_low_fn,
-                NULL, NULL, NULL, PRIO_LOW, 0, 0);
-K_THREAD_DEFINE(thread_med, STACK_SIZE, thread_med_fn,
+
+K_THREAD_DEFINE(thread_a, STACK_SIZE, thread_fn,
                 NULL, NULL, NULL, PRIO_MED, 0, 0);
-K_THREAD_DEFINE(thread_high, STACK_SIZE, thread_high_fn,
-                NULL, NULL, NULL, PRIO_HIGH, 0, 0);
-K_THREAD_DEFINE(thread_cooperative, STACK_SIZE, thread_cooperative_fn,
-                NULL, NULL, NULL, PRIO_COOP, 0, 0);
+
+K_THREAD_DEFINE(thread_b, STACK_SIZE, thread_fn,
+                NULL, NULL, NULL, PRIO_MED, 0, 0);
 
                 
 int main(void)
 {
-    LOG_INF("=== L1 TASK 1: Thread Interleaving ===");
-    LOG_INF("Thread LOW: priority %d, sleeps 300ms", PRIO_LOW);
-    LOG_INF("Thread MED: priority %d, sleeps 200ms", PRIO_MED);
-    LOG_INF("Thread HIGH: priority %d, sleeps 100ms", PRIO_HIGH);
-    LOG_INF("Thread COOPERATIVE: priority %d, sleeps 100ms", PRIO_COOP);
+    k_mutex_init(&shared_mutex);
+    shared_counter = 0;
+    LOG_INF("=== L2 TASK 1: Observe and fix a real race condition ===");
     return 0;
 }
